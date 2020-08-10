261ea177de3bd82f0ac9887479a6873f02a39511
Balaji S Prabakaran <balaji.prabakaran@tigerspike.com>
2020-08-10 23:44:36 +0530

    Second push

--- src/pages/BookDetails.js
+++ src/pages/BookDetails.js
@@ -13,7 +13,7 @@ const BookDetails = () => {
     return book.id === id;
   });
   if (!book) {
-    return <h3>Loading...</h3>;
+    return <h3>Initial loading.....</h3>;
   }
 
   const { image: url, title, description, author, price } = book;
